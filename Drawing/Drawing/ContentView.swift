6624738fee1ba83b3f38875ed10e2df192c3ddd0
liyicky <jason@liyicky.com>
2022-07-18 20:59:02 +0900

    day 44

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -9,46 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @Namespace var safeSpace
-    @State var animationStarted = false
-    
-    var body: some View {
-        ZStack {
-            Circle()
-                .fill(.white)
-                .frame(width: 150)
-                .zIndex(1)
-                .offset(x: -100, y: animationStarted ? -320 : -450)
-            LinearGradient(gradient: Gradient(colors: [.green, .blue]), startPoint: .top, endPoint: .bottom)
-                .frame(width: 150)
-                .clipShape(Circle())
-                .offset(x: -100, y: -300)
-            
-            LinearGradient(gradient: Gradient(colors: [.green, .blue]), startPoint: .top, endPoint: .bottom)
-                .clipShape(Circle())
-                .frame(width: 150)
-                .offset(x: 100, y: -300)
-            //
-            
-            
-            Arc(startAngle: .degrees(90), endAngle: .degrees(270), clockwise: true)
-                .strokeBorder(LinearGradient(gradient: Gradient(colors: [.green, .blue]), startPoint: .leading, endPoint: .trailing), lineWidth: 100)
-                .offset(y: -150)
-                
-        }
-        .onAppear {
-            withAnimation(.easeInOut(duration: 0.3)) {
-                animationStarted = true
+    @State private var colorCycle = 0.0
+
+        var body: some View {
+            ZStack {
+                Image("Example")
+
+                Rectangle()
+                    .fill(.green)
+                    .blendMode(.multiply)
             }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                withAnimation(.easeInOut(duration: 0.3)) {
-                    animationStarted = false
-                }
-            }
+            .frame(width: 400, height: 500)
+            .clipped()
         }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -61,15 +36,15 @@ struct Arc: Shape, InsettableShape {
     var endAngle: Angle
     var clockwise: Bool
     var insetAmount = 0.0
-
+    
     func path(in rect: CGRect) -> Path {
         let rotationAdjustment = Angle.degrees(90)
         let modifiedStart = startAngle - rotationAdjustment
         let modifiedEnd = endAngle - rotationAdjustment
-
+        
         var path = Path()
         path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-
+        
         return path
     }
     
@@ -83,12 +58,82 @@ struct Arc: Shape, InsettableShape {
 struct Triangle: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
-
+        
         path.move(to: CGPoint(x: rect.midX, y: rect.minY))
         path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
         path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-
+        
         return path
+    }
+}
+
+struct Flower: Shape {
+    // How much to move this petal away from the center
+    var petalOffset: Double = -20
+    
+    // How wide to make each petal
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        // The path that will hold all petals
+        var path = Path()
+        
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+            
+            // add it to our main path
+            path.addPath(rotatedPetal)
+        }
+        
+        // now send the main path back
+        return path
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
