0feb7015ac2f4b38c3f5bc3dc5f262e20d2f9ebf
liyicky <jason@liyicky.com>
2022-07-19 22:29:22 +0900

    day 46

---
+++ Drawing/Drawing/Arrow.swift
@@ -0,0 +1,10 @@
+//
+//  Arrow.swift
+//  Drawing
+//
+//  Created by Jason Cheladyn on 2022/07/19.
+//
+
+import SwiftUI
+
+
--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -9,20 +9,93 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var colorCycle = 0.0
-
-        var body: some View {
-            ZStack {
-                Image("Example")
-
-                Rectangle()
-                    .fill(.green)
-                    .blendMode(.multiply)
-            }
-            .frame(width: 400, height: 500)
-            .clipped()
+    @State private var thickness: Double = 0
+    @State private var offset: Double = 0
+    
+    var body: some View {
+        
+        VStack {
+            
+            Arrow(thickness: thickness)
+                .fill(.green)
+                .frame(width: 300, height: 300)
+                .blendMode(.screen)
+                .onTapGesture {
+                    withAnimation(.interpolatingSpring(stiffness: 50, damping: 5)) {
+                        
+                        thickness = 65
+                    }
+                }
+            
+            Slider(value: $thickness, in: -50...100)
+                .padding()
+            
+            ColorCyclingRectangle(offset: offset)
+                .frame(width: 300, height: 200)
+            
+            Slider(value: $offset, in: -1...1)
+                .padding()
+                
         }
+        .background(
+            Color.black
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .ignoresSafeArea())
+//        Checkerboard(rows: rows, columns: columns)
+//            .onTapGesture {
+//                withAnimation(.linear(duration: 10)) {
+//                    rows = 800
+//                    columns = 1600
+//                }
+//            }
+//            .ignoresSafeArea()
+        //
+//        Trapezoid(insetAmount: insetAmount)
+//            .frame(width: 200, height: 100)
+//            .onTapGesture {
+//                withAnimation {
+//                    insetAmount = Double.random(in: 10...90)
+//                }
+//            }
+        
+        /*
+        VStack {
+            Image("Example")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .saturation(amount)
+                .blur(radius: (1 - amount) * 20)
+            
+            ZStack {
+                Circle()
+                    .fill(Color(red: 1, green: 0, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(Color(red:0, green: 1, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(Color(red:0, green: 0, blue: 1))
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
+         */
     }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
@@ -105,7 +178,7 @@ struct Flower: Shape {
 struct ColorCyclingCircle: View {
     var amount = 0.0
     var steps = 100
-
+    
     var body: some View {
         ZStack {
             ForEach(0..<steps) { value in
@@ -126,14 +199,151 @@ struct ColorCyclingCircle: View {
         }
         .drawingGroup()
     }
-
+    
     func color(for value: Int, brightness: Double) -> Color {
         var targetHue = Double(value) / Double(steps) + amount
-
+        
         if targetHue > 1 {
             targetHue -= 1
         }
-
+        
         return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
+
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var offset: Double
+    var thing1: UnitPoint {
+        UnitPoint(x: -offset, y: -offset)
+    }
+    var thing2: UnitPoint {
+        UnitPoint(x:offset, y: offset)
+    }
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(gradient: Gradient(colors: [
+                            color(for: value, brightness: 1),
+                            color(for: value, brightness: 0.5)
+                        ]),
+                                       startPoint: thing1, endPoint: thing2
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to:    CGPoint(x: 0,                       y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount,             y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX,               y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0,                       y: rect.maxY))
+        
+        return path
+    }
+}
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
+        return path
+    }
+}
+
+struct Arrow: Shape {
+    
+    var thickness: Double
+    
+    var animatableData: Double {
+        get { thickness }
+        set { thickness = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to:    CGPoint(x: rect.midX, y: rect.minY)) // Tip of the point
+        // Slanting right of the arrow
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+        // Underside right of the arrow
+        path.addLine(to: CGPoint(x: rect.maxX * 0.66+thickness, y: rect.midY))
+        // Vertical line right of the arrow
+        path.addLine(to: CGPoint(x: rect.maxX * 0.66+thickness, y: rect.maxY))
+//        // Bottomw line of the arrow
+        path.addLine(to: CGPoint(x: rect.maxX * 0.33-thickness, y: rect.maxY))
+//        // Vertical line Left of the arrow
+        path.addLine(to: CGPoint(x: rect.maxX * 0.33-thickness, y: rect.midY))
+//        // Underside left of the arrow
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+//        // Slanting left of the arrow
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+//
