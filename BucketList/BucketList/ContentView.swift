e97e9d6bf8d821db61065eb1fa9b7c78b181eaa4
liyicky <jason@liyicky.com>
2022-12-02 12:54:58 +0900

    day 71 (Bucket List: 4)

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
             
             
             VStack {
+                tmpHud
                 buttonHud
                 
             }
--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 struct EditView: View {
     @Environment(\.dismiss) var dismiss
     var location: Location
+    
+    enum LoadingState {
+        case loading, loaded, failed
+    }
 
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
     @State private var name: String
     @State private var description: String
     
@@ -23,6 +29,29 @@ struct EditView: View {
     }
     
     var onSave: (Location) -> Void
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // we got some data back!
+            let items = try JSONDecoder().decode(Result.self, from: data)
+
+            // success – convert the array values to our pages array
+            pages = items.query.pages.values.sorted()
+            loadingState = .loaded
+        } catch {
+            // if we're still here it means the request failed somehow
+            loadingState = .failed
+        }
+    }
 
     var body: some View {
         NavigationView {
@@ -30,6 +59,23 @@ struct EditView: View {
                 Section {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
+                }
+                
+                Section("Nearby…") {
+                    switch loadingState {
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text(page.description)
+                                .italic()
+                        }
+                    case .loading:
+                        Text("Loading…")
+                    case .failed:
+                        Text("Please try again later.")
+                    }
                 }
             }
             .navigationTitle("Place details")
@@ -43,6 +89,9 @@ struct EditView: View {
                     onSave(newLocation)
                     dismiss()
                 }
+            }
+            .task {
+                await fetchNearbyPlaces()
             }
         }
     }
